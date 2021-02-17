22c1b9dad80a572b94d141a6b21c001691b1a546
Aimreh <77749055+Aimreh@users.noreply.github.com>
2021-02-17 14:43:44 +0000

    init commit

---
+++ JavaScript/Main.js
@@ -0,0 +1,29 @@
+﻿import * as SCENE from "./GameLogic/SceneRendering.js";
+// Main function
+
+main();
+
+function main()
+{
+    // Load materials used in the game
+    function loadContent() { 
+        // Add content logic here
+    }
+
+    // Add initial objects to the scene
+    function addToScene() {
+        // Add scene logic here
+    }
+
+    // Animation loop. Three.js has both update and draw in the same function.
+    function animate() {
+        mainUpdate();
+    }
+
+    animatee();
+
+}
+
+function mainUpdate() {
+    // Add update logic here
+}
